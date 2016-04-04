8c7b935f2d4cd82baae3ee1f974f0c2f44522545
snavien <smvien@ucdavis.edu>
2016-04-04 04:27:50 -0700

    adding up times

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ import unicodedata
 def load_jsons():
     sixty_stack = []
     with open(os.getcwd() + '/data-gen/tweets.txt ', 'r', encoding='utf-8') as j_file:
+         json_block = []
          init_time = 0
          for line in j_file:
 
             json_dict = json.loads('['+line+']')
             try:
-                init_time = json_dict[0]['created_at'].split(' ')[3].split(':')[2])
+                print(json_dict[0]['created_at'].split(' ')[3])
+                time = json_dict[0]['created_at'].split(' ')[3]
+                init_time = int(time.split(':')[0])*60 * 60 + int(time.split(':')[1])*60 + int(time.split(':')[0])
+                print(init_time)
                 print(json_dict[0]['entities']['hashtags'])
             except KeyError:
                 print(json_dict)
