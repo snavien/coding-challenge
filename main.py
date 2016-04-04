17e5ba4572cb8274827a44bfa82b4b9d4be8c544
snavien <smvien@ucdavis.edu>
2016-04-04 14:00:44 -0700

    choosing checks; created class for hashtags, need combinations

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import json
 import os
+import itertools
 
 class ht:
-    def __init__(self):
-        self.hashtag_pair = []
-        self.first_degree = 0
+    def __init__(self, time, text, indices):
+        self.text = text
+        self.indices = indices
+        self.time = time
 
 
 def load_jsons():
@@ -12,6 +14,8 @@ def load_jsons():
     avg_degree = 0
     hashtag_set = set()
     time_set = False
+
+
     with open(os.getcwd() + '/data-gen/tweets.txt ', 'r', encoding='utf-8') as j_file:
          json_block = []
          init_time = 0
@@ -19,34 +23,50 @@ def load_jsons():
 
             json_dict = json.loads('['+line+']')
             try:
-                print(json_dict[0]['created_at'].split(' ')[3])
                 time = json_dict[0]['created_at'].split(' ')[3]
                 time = int(time.split(':')[0])*60 * 60 + int(time.split(':')[1])*60 + int(time.split(':')[0])
-                print(time)
-                print(json_dict[0]['entities']['hashtags'])
+                # print(time)
+                # print(json_dict[0]['entities']['hashtags'])
                 curr_ht = json_dict[0]['entities']['hashtags']
+
+
                 if init_time == 0:
                     init_time = time
                     time_set = True
                     num_elem = len(curr_ht)
-                    print(num_elem)
-                    print(curr_ht)
+                    # print(num_elem)
+                    # print(curr_ht)
 
+                    build_window(hashtag_q, curr_ht, avg_degree)
 
-                    if len(hashtag_q) >= 1 and num_elem >= 1:   #has items to update
-                        hashtag_q.pop(0)                        #pop old
+                if time_set:
+                    if time - init_time < 60 and time - init_time >= 0:
+                        build_window(hashtag_q, curr_ht, avg_degree)
+                    elif time - init_time == 60:
+                        build_window(hashtag_q)
                     else:
-
-                # if time_set:
-                #     if time - init_time < 60:
-                #         sixty_stack
-                #     elif time - init_time == 60:
-                #
+                        if time - init_time < 0:
+                            pop_old(hashtag_q, hashtag_set, curr_ht, avg_degree)
 
 
             except KeyError:
                 print(json_dict)
 
+def build_window(hashtag_q, hashtag_set, curr_ht, avg_degree):
+    len_curr_q = 0
+    for h in curr_ht:
+        hashtag = ht(time, h['text'], h['indices'])
+        print(hashtag.text)
+        hashtag_q.append(hashtag)
+        if hashtag.text in hashtag_set:
+            len_curr_q += 1
+        hashtag_set.add(hashtag.text)
+    return len_curr_q
+
+def pop_old(hashtag_q, hashtag_set, curr_ht, avg_degree):
+    if len(hashtag_q) > 0 and num_elem <= 1:  # has items to update
+        old = hashtag_q.pop(0)  # pop old
+        avg_degree - ((old.indices[0] + old.indices[1]) / 2.0)
 
 
 load_jsons()
