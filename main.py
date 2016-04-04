92599c4db427781ce855cac1c398bf9c43eabbcb
snavien <smvien@ucdavis.edu>
2016-04-04 10:39:47 -0700

    figuring out how to update windows of time

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import json
 import os
-import unicodedata
+
+class ht:
+    def __init__(self):
+        self.hashtag_pair = []
+        self.first_degree = 0
 
 
 def load_jsons():
-    sixty_stack = []
+    hashtag_q = []
+    avg_degree = 0
+    hashtag_set = set()
+    time_set = False
     with open(os.getcwd() + '/data-gen/tweets.txt ', 'r', encoding='utf-8') as j_file:
          json_block = []
          init_time = 0
@@ -14,9 +21,29 @@ def load_jsons():
             try:
                 print(json_dict[0]['created_at'].split(' ')[3])
                 time = json_dict[0]['created_at'].split(' ')[3]
-                init_time = int(time.split(':')[0])*60 * 60 + int(time.split(':')[1])*60 + int(time.split(':')[0])
-                print(init_time)
+                time = int(time.split(':')[0])*60 * 60 + int(time.split(':')[1])*60 + int(time.split(':')[0])
+                print(time)
                 print(json_dict[0]['entities']['hashtags'])
+                curr_ht = json_dict[0]['entities']['hashtags']
+                if init_time == 0:
+                    init_time = time
+                    time_set = True
+                    num_elem = len(curr_ht)
+                    print(num_elem)
+                    print(curr_ht)
+
+
+                    if len(hashtag_q) >= 1 and num_elem >= 1:   #has items to update
+                        hashtag_q.pop(0)                        #pop old
+                    else:
+
+                # if time_set:
+                #     if time - init_time < 60:
+                #         sixty_stack
+                #     elif time - init_time == 60:
+                #
+
+
             except KeyError:
                 print(json_dict)
 
