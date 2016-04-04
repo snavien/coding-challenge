5acefc97363e68be6aa765a5cc6f2be21d4a8494
snavien <smvien@ucdavis.edu>
2016-04-04 16:42:09 -0700

    added degree check now to create sliding window

--- main.py
+++ main.py
@@ -1,72 +1,95 @@
 import json
 import os
-import itertools
-
+from itertools import combinations    
 class ht:
-    def __init__(self, time, text, indices):
+    def __init__(self, time, text):
         self.text = text
-        self.indices = indices
         self.time = time
+        self.degree = 1
+    def inc_degree(self):
+        self.degree += 1
+
+class edge:
+    def __init__(self, pair, indices):
+        self.pair = pair
+
+def build_window(curr_ht, time, ht_set, avg_degree, num_nodes, ht_q):
+    text_list = []
+    edge_list = []
+    for h in curr_ht:
+        hashtag = ht(time, h['text'])
+        text_list.append(hashtag.text)
+
+    comb = combinations(text_list, 2)
+    for c in comb:
+        if len(c) == 2:
+            edge_list.append(c)
+            if c not in ht_set:
+                ht_set.add(c)   #adding a check for edges already in the graph
+                num_nodes += 1  #incrementing nodes for each edge that is added
+                make_degree(c[0], ht_q, time)
+                make_degree(c[1], ht_q, time)
+
+    print(edge_list)
+
+def make_degree(text, ht_q, time):
+    text = str(text)
+    if any(x.time != time and x.text != text for x in ht_q):
+        ht_q.append(ht(time, text))
+    else:
+        for x in ht_q:
+            if x.time == time and x.text == text:
+                x.inc_degree()
+
+# def pop_old(hashtag_q, hashtag_set, curr_ht, avg_degree):
+#     if len(hashtag_q) > 0 and num_elem <= 1:  # has items to update
+#         old = hashtag_q.pop(0)  # pop old
+#         avg_degree - ((old.indices[0] + old.indices[1]) / 2.0)
+
 
 
 def load_jsons():
-    hashtag_q = []
+    ht_q = []
     avg_degree = 0
     hashtag_set = set()
     time_set = False
 
-
     with open(os.getcwd() + '/data-gen/tweets.txt ', 'r', encoding='utf-8') as j_file:
-         json_block = []
          init_time = 0
+         num_nodes = 0
+         ht_q = []
          for line in j_file:
 
             json_dict = json.loads('['+line+']')
             try:
                 time = json_dict[0]['created_at'].split(' ')[3]
                 time = int(time.split(':')[0])*60 * 60 + int(time.split(':')[1])*60 + int(time.split(':')[0])
-                # print(time)
-                # print(json_dict[0]['entities']['hashtags'])
                 curr_ht = json_dict[0]['entities']['hashtags']
 
+                ht_set = set()
 
                 if init_time == 0:
                     init_time = time
                     time_set = True
-                    num_elem = len(curr_ht)
-                    # print(num_elem)
-                    # print(curr_ht)
 
-                    build_window(hashtag_q, curr_ht, avg_degree)
+                    build_window(curr_ht, time, ht_set, avg_degree, num_nodes, ht_q)
 
-                if time_set:
-                    if time - init_time < 60 and time - init_time >= 0:
-                        build_window(hashtag_q, curr_ht, avg_degree)
-                    elif time - init_time == 60:
-                        build_window(hashtag_q)
-                    else:
-                        if time - init_time < 0:
-                            pop_old(hashtag_q, hashtag_set, curr_ht, avg_degree)
+
+                    #     build_window(hashtag_q, curr_ht, avg_degree, time)
+                #
+                # if time_set:
+                #     if time - init_time < 60 and time - init_time >= 0:
+                #         build_window(hashtag_q, curr_ht, avg_degree, time)
+                #     elif time - init_time == 60:
+                #         build_window(hashtag_q, curr_ht, avg_degree, time)
+                #     else:
+                #         continue
+                #         # if time - init_time < 0:
+                #         #     pop_old(hashtag_q, hashtag_set, curr_ht, avg_degree)
 
 
             except KeyError:
-                print(json_dict)
-
-def build_window(hashtag_q, hashtag_set, curr_ht, avg_degree):
-    len_curr_q = 0
-    for h in curr_ht:
-        hashtag = ht(time, h['text'], h['indices'])
-        print(hashtag.text)
-        hashtag_q.append(hashtag)
-        if hashtag.text in hashtag_set:
-            len_curr_q += 1
-        hashtag_set.add(hashtag.text)
-    return len_curr_q
-
-def pop_old(hashtag_q, hashtag_set, curr_ht, avg_degree):
-    if len(hashtag_q) > 0 and num_elem <= 1:  # has items to update
-        old = hashtag_q.pop(0)  # pop old
-        avg_degree - ((old.indices[0] + old.indices[1]) / 2.0)
+                continue
 
 
 load_jsons()
